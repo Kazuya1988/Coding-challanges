63f200a8e34a85c5d65b286d16b3ceaa19299509
Stefan <stefankazuyagrbic88@gmail.com>
2022-11-10 17:31:38 +0100

    Solved second coding challenge from 11-10-2022

--- CC 11-10-2022/app.js
+++ CC 11-10-2022/app.js
@@ -9,5 +9,21 @@
 function solution(str, ending) {
 	return str.endsWith(ending);
 }
-console.log(solution('abc', 'bc'));
-console.log(solution('abc', 'd'));
+// console.log(solution('abc', 'bc'))
+// console.log(solution('abc', 'd'))
+
+// Is this a triangle?
+
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+
+function isTriangle(a, b, c) {
+	let p = (a + b + c) / 2;
+	if (p <= a || p <= b || p <= c) return false;
+	else return true;
+	// Other solution
+	// return a + b > c && a + c > b && c + b > a;
+}
+
+console.log(isTriangle(7, 2, 2));
