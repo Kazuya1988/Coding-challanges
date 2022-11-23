bc17365a3adae23724acaea572b5cc7f1cb228e2
Stefan <stefankazuyagrbic88@gmail.com>
2022-11-23 21:01:09 +0100

    Solved second coding challenge from 11-23-2022

--- CC 11-23-2022/app.js
+++ CC 11-23-2022/app.js
@@ -8,3 +8,26 @@ function breakChocolate(n, m) {
 	return n && m ? n * m - 1 : 0;
 }
 console.log(breakChocolate(5, 5));
+
+// Find the stray number
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+// Complete the method which accepts such an array, and returns that single different number.
+
+// The input array will always be valid! (odd-length >= 3)
+
+// Examples
+// [1, 1, 2] ==> 2
+// [17, 17, 3, 17, 17, 17, 17] ==> 3
+function stray(numbers) {
+	// Solution 1 : Clever - using (^) is a bitwise XOP operator and compares the numbers at a binary level
+	// Because the array is odd-length, all equal pairs cancel each other. The only number remaining is the stray number.
+	// return numbers.reduce((a,b) => a ^ b);
+	// Solution 2
+	for (var i in numbers) {
+		if (numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])) {
+			return numbers[i];
+		}
+	}
+}
+console.log(stray([17, 17, 3, 17, 17, 17, 17]));
