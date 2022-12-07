7067b93317e7643c890c5ffd01d2519e3ac5f9d7
Stefan <stefankazuyagrbic88@gmail.com>
2022-12-07 16:39:42 +0100

    Solved second coding challenge from 12-07-2022

--- CC 12-07-2022/app.js
+++ CC 12-07-2022/app.js
@@ -6,4 +6,30 @@
 function smallEnough(a, limit) {
 	return a.every((e) => e <= limit);
 }
-console.log(smallEnough([66, 101], 200));
+// console.log(smallEnough([66, 101], 200));
+
+// Largest 5 digit number in a series
+// In the following 6 digit number:
+
+// 283910
+// 91 is the greatest sequence of 2 consecutive digits.
+
+// In the following 10 digit number:
+
+// 1234567890
+// 67890 is the greatest sequence of 5 consecutive digits.
+
+// Complete the solution so that it returns the greatest sequence of five consecutive digits found within the number given. The number will be passed in as a string of only digits. It should return a five digit integer. The number passed may be as large as 1000 digits.
+
+// Adapted from ProjectEuler.net
+
+function solution(digits) {
+	let splittedDig = digits.split('');
+	let fiveDig = [];
+	for (let i = 0; i < splittedDig.length; i++) {
+		fiveDig.push(splittedDig.slice(i, 5 + i));
+	}
+	let fiveDigNums = fiveDig.map((e) => Number(e.join('')));
+	return Math.max(...fiveDigNums);
+}
+console.log(solution('1234567898765'));
