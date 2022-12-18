81006c78f3915e755b897b95210c8b9e18a58cad
Stefan <stefankazuyagrbic88@gmail.com>
2022-12-18 13:23:21 +0100

    Solved third coding challenge from 12-18-2022

--- CC 12-18-2022/app.js
+++ CC 12-18-2022/app.js
@@ -31,3 +31,20 @@ function minValue(values) {
 	return [...new Set(values)].sort().join('');
 }
 console.log(minValue([1, 9, 3, 1, 7, 4, 6, 6, 7]));
+
+// Flatten and sort an array
+// Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+
+// Example:
+
+// Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+// Addendum:
+
+// Please, keep in mind, that JavaScript is by default sorting objects alphabetically.
+('use strict');
+
+function flattenAndSort(array) {
+	return array.flat().sort((a, b) => a - b);
+}
+console.log(flattenAndSort([[3, 2, 1], [4, 6, 5], [], [9, 7, 8]]));
