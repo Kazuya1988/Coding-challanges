4187608680f6096e2cbbd827d0b066f620c1f824
Stefan <stefankazuyagrbic88@gmail.com>
2023-01-08 20:50:25 +0100

    Solved second coding challenge from 01-08-2023

--- CC 01-08-2023/app.js
+++ CC 01-08-2023/app.js
@@ -11,3 +11,24 @@ function largestPairSum(numbers) {
 	return sortedNumbers[0] + sortedNumbers[1];
 }
 console.log(largestPairSum([10, 14, 2, 23, 19]));
+
+// Most digits
+// Find the number with the most digits.
+// If two numbers in the argument array have the same number of digits, return the first one in the array.
+function findLongest(array) {
+	// let largestNumber = array[0];
+	// for (let i = 1; i < array.length; i++) {
+	//   if (array[i].toString().length > largestNumber.toString().length) {
+	//     largestNumber = array[i];
+	//   }
+	// }
+	// return largestNumber;
+	const longestNumbers = array.filter((number) => {
+		return (
+			number.toString().length ===
+			Math.max(...array.map((number) => number.toString().length))
+		);
+	});
+	return longestNumbers[0];
+}
+console.log(findLongest([10, 500, 900]));
