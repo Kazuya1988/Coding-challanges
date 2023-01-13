ba8fbdf5b4cd20461242b6239aea8216f8f6f7ee
Stefan <stefankazuyagrbic88@gmail.com>
2023-01-13 15:21:09 +0100

    Solved second coding challenge from 01-13-2023

--- CC 01-13-2023/app.js
+++ CC 01-13-2023/app.js
@@ -10,3 +10,21 @@ function evenNumbers(array, number) {
 	return array.filter((num) => num % 2 === 0).slice(-number);
 }
 console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3));
+
+// Currying functions: multiply all elements in an array
+// To complete this Kata you need to make a function multiplyAll/multiply_all which takes an array of integers as an argument. This function must return another function, which takes a single integer as an argument and returns a new array.
+
+// The returned array should consist of each of the elements from the first array multiplied by the integer.
+
+// Example:
+
+// multiplyAll([1, 2, 3])(2) = [2, 4, 6];
+// You must not mutate the original array.
+function multiplyAll(arr) {
+	return function (n) {
+		let newArr = [];
+		arr.forEach((num) => newArr.push(num * n));
+		return newArr;
+	};
+}
+console.log(multiplyAll([1, 2, 3])(2));
