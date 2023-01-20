776193f79e030fd372af65aac968e228f95e716c
Stefan <stefankazuyagrbic88@gmail.com>
2023-01-20 16:51:27 +0100

    Solved coding challenge from 01-20-2023

---
+++ CC 01-20-2023/app.js
@@ -0,0 +1,17 @@
+// Halving Sum
+// Task
+// Given a positive integer n, calculate the following sum:
+
+// n + n/2 + n/4 + n/8 + ...
+// All elements of the sum are the results of integer division.
+
+// Example
+// 25  =>  25 + 12 + 6 + 3 + 1 = 47
+function halvingSum(n) {
+	let sum = 0;
+	for (let i = 1; i <= 25; i *= 2) {
+		sum += Math.floor(n / i);
+	}
+	return sum;
+}
+console.log(halvingSum(25));
