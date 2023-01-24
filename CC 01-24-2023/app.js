b84fe05ff47413d6baaed3b8322972bf397a9d48
Stefan <stefankazuyagrbic88@gmail.com>
2023-01-24 13:32:33 +0100

    Solved second coding challenge from 01-24-2023

--- CC 01-24-2023/app.js
+++ CC 01-24-2023/app.js
@@ -7,4 +7,53 @@
 const flatten = function (array) {
 	return [].concat(...array);
 };
-console.log(flatten([[[1, 2, 3]]]));
+// console.log(flatten([[[1, 2, 3]]]));
+
+// Moves in squared strings (I)
+
+// You are given a string of n lines, each substring being n characters long: For example:
+
+// s = "abcd\nefgh\nijkl\nmnop"
+
+// We will study some transformations of this square of strings.
+
+// Vertical mirror: vert_mirror (or vertMirror or vert-mirror)
+// vert_mirror(s) => "dcba\nhgfe\nlkji\nponm"
+// Horizontal mirror: hor_mirror (or horMirror or hor-mirror)
+//  hor_mirror(s) => "mnop\nijkl\nefgh\nabcd"
+// or printed:
+
+// vertical mirror   |horizontal mirror
+// abcd --> dcba     |abcd --> mnop
+// efgh     hgfe     |efgh     ijkl
+// ijkl     lkji     |ijkl     efgh
+// mnop     ponm     |mnop     abcd
+// Task:
+// Write these two functions
+// and
+
+// high-order function oper(fct, s) where
+
+// fct is the function of one variable f to apply to the string s (fct will be one of vertMirror, horMirror)
+
+// Examples:
+let s = 'abcd\nefgh\nijkl\nmnop';
+// oper(vert_mirror, s) => "dcba\nhgfe\nlkji\nponm"
+// oper(hor_mirror, s) => "mnop\nijkl\nefgh\nabcd"
+// Note:
+// The form of the parameter fct in oper changes according to the language. You can see each form according to the language in "Sample Tests".
+
+function vertMirror(strng) {
+	return strng
+		.split('\n')
+		.map((s) => s.split('').reverse().join(''))
+		.join('\n');
+}
+// console.log(horMirror("abcd\nefgh\nijkl\nmnop"))
+function horMirror(strng) {
+	return strng.split('\n').reverse().join('\n');
+}
+function oper(fct, s) {
+	return fct(s);
+}
+console.log(oper(vertMirror, s));
