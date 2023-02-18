b05cec47a40734d1c7f30dcf69a9e187d25ad55d
Stefan <stefankazuyagrbic88@gmail.com>
2023-02-18 22:44:45 +0100

    Solved coding challenge from 02-18-2023

---
+++ CC 02-18-2023/app.js
@@ -0,0 +1,22 @@
+// Highest Scoring Word
+// Given a string of words, you need to find the highest scoring word.
+
+// Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+// For example, the score of abad is 8 (1 + 2 + 1 + 4).
+
+// You need to return the highest scoring word as a string.
+
+// If two words score the same, return the word that appears earliest in the original string.
+
+// All letters will be lowercase and all inputs will be valid.
+function high(x) {
+	const alpha = 'abcdefghijklmnopqrstuvwxyz';
+	const words = x.split(' ');
+	const scores = words
+		.map((x) => [...x].map((y) => alpha.indexOf(y) + 1))
+		.map((x) => x.reduce((a, b) => a + b, 0));
+
+	return words[scores.indexOf(Math.max(...scores))];
+}
+console.log(high('man i need a taxi up to ubud'));
