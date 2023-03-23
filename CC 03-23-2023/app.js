f726929b991ed52341ad3fce95a2d2cff14305e6
Stefan <stefankazuyagrbic88@gmail.com>
2023-03-23 18:54:38 +0100

    Solved second coding challenge from 03-23-2023

--- CC 03-23-2023/app.js
+++ CC 03-23-2023/app.js
@@ -56,3 +56,32 @@ console.log(
 		]
 	)
 );
+
+// Strings
+// Write Number in Expanded Form
+// Write Number in Expanded Form
+// You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+// expandedForm(12); // Should return '10 + 2'
+// expandedForm(42); // Should return '40 + 2'
+// expandedForm(70304); // Should return '70000 + 300 + 4'
+// NOTE: All numbers will be whole numbers greater than 0.
+function expandedForm(num) {
+	const numbers = String(num).split('');
+	const result = [];
+	for (let i = 0; i < numbers.length; i++) {
+		if (+numbers[i] > 0) {
+			result.push(`${numbers[i]}${'0'.repeat(numbers.length - i - 1)}`);
+		}
+	}
+	return result.join(' + ');
+	// Clever solution
+	//    return num.toString()
+	//                             .split("")
+	//                             .reverse()
+	//                             .map( (a, i) => a * Math.pow(10, i))
+	//                             .filter(a => a > 0)
+	//                             .reverse()
+	//                             .join(" + ");
+}
+console.log(expandedForm(70304));
