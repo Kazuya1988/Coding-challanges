187e5748dcb71f165497dff2f5eafdf94665cff9
Stefan <stefankazuyagrbic88@gmail.com>
2023-04-14 20:43:07 +0200

    Solved secoind coding challenge from 04-14-2023

--- CC 04-14-2023/app.js
+++ CC 04-14-2023/app.js
@@ -7,3 +7,14 @@ function spacify(str) {
 	return str.split('').join(' ');
 }
 console.log(spacify('hello world'));
+
+// Strings
+// All unique
+// Write a program to determine if a string contains only unique characters. Return true if it does and false otherwise.
+
+// The string may contain any of the 128 ASCII characters. Characters are case-sensitive, e.g. 'a' and 'A' are considered different characters.
+function hasUniqueChars(str) {
+	return str.length === [...new Set(str)].length;
+	// return str.length === new Set(str).size
+}
+console.log(hasUniqueChars('abcdef'));
