d62e39cbc7a2680b474e4f63f84ef6244073899b
Stefan <stefankazuyagrbic88@gmail.com>
2023-08-18 17:02:12 +0200

    Solved second coding challenge from 08-18-2023

--- CC 08-18-2023/app.js
+++ CC 08-18-2023/app.js
@@ -20,3 +20,39 @@ function vowelBack(s) {
 	return result;
 }
 console.log(vowelBack('testcase'));
+
+// Basics 03: Strings, Numbers and Calculation
+// Here you have to do some mathematical operations on a "dirty string". This kata checks some basics, it's not too difficult.
+
+// So what to do?
+
+// Input: String which consists of two positive numbers (doubles) and exactly one operator like +, -, * or / always between these numbers. The string is dirty, which means that there are different characters inside too, not only numbers and the operator. You have to combine all digits left and right, perhaps with "." inside (doubles), and to calculate the result which has to be rounded to an integer and converted to a string at the end.
+
+// Easy example:
+// Input: "gdfgdf234dg54gf*23oP42"
+// Output: "54929268" (because 23454*2342=54929268)
+// First there are some static tests, later on random tests too...
+function calculateString(st) {
+	let filteredString = st.match(/[0-9*+\-/\.]/g).join('');
+
+	let numbers = filteredString.split(/[\+\-\*\/]/);
+	let operators = filteredString.match(/[\+\-\*\/]/g);
+
+	let result = parseFloat(numbers[0]);
+	for (let i = 1; i < numbers.length; i++) {
+		if (operators[i - 1] === '+') {
+			result += parseFloat(numbers[i]);
+		} else if (operators[i - 1] === '-') {
+			result -= parseFloat(numbers[i]);
+		} else if (operators[i - 1] === '*') {
+			result *= parseFloat(numbers[i]);
+		} else if (operators[i - 1] === '/') {
+			result /= parseFloat(numbers[i]);
+		}
+	}
+
+	return String(Math.round(result));
+}
+console.log(calculateString('gdfgdf234dg54gf*23oP42'));
+console.log(calculateString(';$%§fsdfsd235??df/sdfgf5gh.000kk0000'));
+console.log(calculateString('fsdfsd235???34.4554s4234df-sdfgf2g3h4j442'));
