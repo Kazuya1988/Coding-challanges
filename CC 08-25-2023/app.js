9c738424796748e5d13dc72d1e14026d486778f1
Stefan <stefankazuyagrbic88@gmail.com>
2023-08-25 18:00:21 +0200

    Solved coding challenge from 08-25-2023

--- CC 08-25-2023/app.js
+++ CC 08-25-2023/app.js
@@ -1,3 +1,4 @@
+// Arrays
 // Pluck
 // Implement a function which takes a sequence of objects and a property name, and returns a sequence containing the named property of each object.
 
@@ -11,3 +12,20 @@ function pluck(objs, name) {
 }
 console.log(pluck([{ a: 1 }, { a: 2 }], 'a'));
 console.log(pluck([{ a: 1, b: 3 }, { a: 2 }], 'b'));
+
+// Organize duplicate numbers in list
+// Sam is an avid collector of numbers. Every time he finds a new number he throws it on the top of his number-pile. Help Sam organise his collection so he can take it to the International Number Collectors Conference in Cologne.
+
+// Given an array of numbers, your function should return an array of arrays, where each subarray contains all the duplicates of a particular number. Subarrays should be in the same order as the first occurence of the number they contain:
+
+// group([3, 2, 6, 2, 1, 3])
+// >>> [[3, 3], [2, 2], [6], [1]]
+// Assume the input is always going to be an array of numbers. If the input is an empty array, an empty array should be returned.
+function group(arr) {
+	if (!arr.length) return [];
+	let remDup = [...new Set(arr)];
+	return remDup.map((e) => {
+		return arr.filter((x) => x === e);
+	});
+}
+console.log(group([3, 2, 6, 2, 1, 3]));
